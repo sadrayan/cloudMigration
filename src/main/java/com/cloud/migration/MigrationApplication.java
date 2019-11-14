ad54c486ac0c29a8f6dac85613368a1f9290b09f
sadra <sadrayan@gmail.com>
2019-11-14 00:55:16 -0500

    cassandra repo

--- src/main/java/com/cloud/migration/MigrationApplication.java
+++ src/main/java/com/cloud/migration/MigrationApplication.java
@@ -2,8 +2,10 @@ package com.cloud.migration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
+@EnableCassandraRepositories
 public class MigrationApplication {
 
 	public static void main(String[] args) {
