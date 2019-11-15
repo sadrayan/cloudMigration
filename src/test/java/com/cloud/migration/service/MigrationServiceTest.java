6a650643eb1f3d2949f6cabfde41324690d21abc
sadra <sadrayan@gmail.com>
2019-11-15 01:22:23 -0500

    added migration service

--- src/test/java/com/cloud/migration/service/MigrationServiceTest.java
+++ src/test/java/com/cloud/migration/service/MigrationServiceTest.java
@@ -1,30 +1,24 @@
 package com.cloud.migration.service;
 
-import com.cloud.migration.controller.CredentialController;
 import com.cloud.migration.enums.MigrationState;
 import com.cloud.migration.exception.WorkloadNotFoundException;
-import com.cloud.migration.model.*;
+import com.cloud.migration.model.Migration;
+import com.cloud.migration.model.TargetCloud;
+import com.cloud.migration.model.Workload;
 import com.cloud.migration.repository.*;
-import com.datastax.driver.core.utils.UUIDs;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
 import java.util.Optional;
-import java.util.UUID;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
